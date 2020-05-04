cec63b260b35b1ff937d7037db8ef4d450834f60
acandael <info@anthonycandaele.com>
2020-05-04 11:58:28 +0200

    implement getCommuteTime

---
+++ client/services/GoogleAPI.js
@@ -0,0 +1,18 @@
+import env from '../config/env.config';
+
+export async function getCommuteTime(origin, destination) {
+  const base = origin;
+  const target = destination;
+
+  let ApiURL = 'https://maps.googleapis.com/maps/api/distancematrix/json?';
+  let params = `origins=${base}&destinations=${target}&key=${env.GOOGLE_API_KEY}`;
+  let finalApiURL = `${ApiURL}${encodeURI(params)}`;
+
+  try {
+    let response = await fetch(finalApiURL);
+    let responseJson = await response.json();
+    return responseJson.rows[0].elements[0].duration.value;
+  } catch (error) {
+    console.error(error);
+  }
+}
