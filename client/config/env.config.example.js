3814ebfc4cb2112d2cb6f84c9132e1d9be53fcaa
acandael <info@anthonycandaele.com>
2020-05-05 17:47:27 +0200

    implement facebook login

---
+++ client/config/env.config.example.js
@@ -0,0 +1,4 @@
+export default {
+  GOOGLE_API_KEY: 'your google api key',
+  FACEBOOK_APP_ID: 'facebook app id',
+};
--- client/config/env.config.js
+++
@@ -1,4 +0,0 @@
-export default {
-  GOOGLE_API_KEY: 'AIzaSyB5ay3d80mbDBSwZlSojmNGzs1N2mG-qek',
-  FACEBOOK_APP_ID: '248045922969013',
-};
