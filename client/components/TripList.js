bb8b3dc881e927a3347a7a2880bba312b14d090e
Nicole Carrion Cogo <nicolecogo@gmail.com>
2020-05-05 17:14:24 +0200

    feat: added TripList page skeleton

---
+++ client/components/TripList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function MyTrips () {
+  return (
+    <View>
+      <Text>My trips</Text>
+    </View>
+  );
+}
