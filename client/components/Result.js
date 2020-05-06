f461530aeb224af70ffb5d6afcd89cd51532c066
Nicole Carrion Cogo <nicolecogo@gmail.com>
2020-05-06 17:24:40 +0200

    refactor: included more data to mock result

--- client/components/Result.js
+++ client/components/Result.js
@@ -1,21 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, SectionList, SafeAreaView, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
 import colorScheme from '../constants/colors';
 
 export default function Result () {
 
-  //TODO use context or redux
-  const result = [
-    {
-      driver: { id: 1, name: 'Virginie' },
-      passengers: [{ id: 2, name: 'Brendan' }, { id: 3, name: 'Anthony' }]
-    },
-    {
-      driver: { id: 4, name: 'John' },
-      passengers: [{ id: 5, name: 'Jane' }, { id: 6, name: 'Jake' }, { id: 7, name: 'Joseph' }]
-    }
-  ];
+  const [result, setResult] = useState([]);
+
+  useEffect(() => {
+    setResult(parseResultToList(mockResult));
+  }, [mockResult]);
 
   function parseResultToList (result) {
     return result.map(({driver, passengers}) => ({
@@ -48,7 +42,7 @@ export default function Result () {
   return (
     <SafeAreaView style={styles.container}>
       <SectionList
-        sections={ parseResultToList(result) }
+        sections={ result }
         renderItem={ ({item}) => Item(item) }
         renderSectionHeader={ ({ section: { title } }) => Header(title) }
       />
@@ -79,3 +73,57 @@ const styles = StyleSheet.create({
     fontSize: 24
   }
 });
+
+//TODO use context or redux
+const mockResult = [
+  {
+    driver: {
+      id: 1, name: 'Virginie',
+      departureDate: 1588774296658,
+      departureLocation: { lat: 41.401648, lng: -2.186230 },
+      departureTime: '16:30:00'
+    },
+    passengers: [
+      {
+        id: 2, name: 'Brendan',
+        departureDate: 1588774296658,
+        departureLocation: { lat: 41.4019693, lng: -2.1860034 },
+        departureTime: '15:45:00'
+      },
+      {
+        id: 3, name: 'Anthony',
+        departureDate: 1588774296658,
+        departureLocation: { lat: 41.4019695, lng: -2.1862589 },
+        departureTime: '16:15:00'
+      },
+      {
+        id: 4, name: 'Lello',
+        departureDate: 1588774296658,
+        departureLocation: { lat: 41.4019542, lng: -2.1862875 },
+        departureTime: '16:05:00'
+      }
+    ]
+  },
+  {
+    driver: {
+      id: 5, name: 'John',
+      departureDate: 1588774443514,
+      departureLocation: { lat: 41.478622, lng: 2.085599 },
+      departureTime: '20:45:00'
+    },
+    passengers: [
+      {
+        id: 6, name: 'Jane',
+        departureDate: 1588774443514,
+        departureLocation: { lat: 41.473568, lng: 2.081234 },
+        departureTime: '19:45:00'
+      },
+      {
+        id: 7, name: 'Jake',
+        departureDate: 1588774443514,
+        departureLocation: { lat: 41.475789, lng: 2.085781 },
+        departureTime: '19:30:00'
+      }
+    ]
+  }
+];
