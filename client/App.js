d1528df4f9c473b55f1b2223eea94bdd00b2f92b
Nicole Carrion Cogo <nicolecogo@gmail.com>
2020-05-07 16:45:37 +0200

    feat: add screen navigation

--- client/App.js
+++ client/App.js
@@ -1,10 +1,30 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Form from './MVPForm/FormLaunch';
+import CarAllocation from './components/CarAllocation';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+export default function App () {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='MVPForm'>
+          <Stack.Screen
+            name='MVPForm'
+            component={Form}
+            options={{title: 'MVP Form'}}
+          />
+          <Stack.Screen
+            name='CarAllocation'
+            component={CarAllocation}
+            options={{title: 'Car Allocation'}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
@@ -13,7 +33,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
 });
