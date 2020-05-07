ac4bf31e73caf87107bc95ed5b60bf96f907f01e
Brendan-G5 <brendan.garvey@queensu.ca>
2020-05-07 11:39:37 +0200

    chore: added details for parseing file

---
+++ client/utils/algoInputParser.js
@@ -0,0 +1,115 @@
+//TODO : change this
+// Array [
+//   Object {
+//   "departureDate": 1588774296658,
+//   "departureLocation": Object {
+//   "lat": 37.4219693,
+//   "lng": -122.0840034,
+//   },
+//   "isAdmin": true,
+//   "isDriver": true,
+//   "name": "ww",
+//   "seats": 3,
+//   },
+//   Object {
+//   "departureLocation": Object {
+//   "lat": 40.656685,
+//   "lng": -4.6812086,
+//   },
+//   "departureTimeStamp": 1588774443514,
+//   "isAdmin": false,
+//   "isDriver": false,
+//   "name": "ii",
+//   "seats": 0,
+//   },
+//   Object {
+//   "departureLocation": Object {
+//   "lat": 37.4219693,
+//   "lng": -122.0840034,
+//   },
+//   "departureTimestamp": 1588861330923,
+//   "isAdmin": true,
+//   "isDriver": true,
+//   "name": "fg",
+//   "seats": 5,
+//   },
+//   ]
+
+
+//TODO: to this  Using the GOOGLE API
+// {
+//   drivers: {
+//   X: {
+//   spots: 2,
+//   departureTime: 10,
+//   },
+//   Y: {
+//   spots: 2,
+//   departureTime: 10,
+//   },
+//   Z: {
+//   spots: 2,
+//   departureTime: 100,
+//   },
+//   },
+//   passengers: {
+//   A: {
+//   drivers: {
+//   X: 1,
+//   Y: 100,
+//   Z: 101,
+//   },
+//   departureTime: 0,
+//   },
+//   B: {
+//   drivers: {
+//   X: 10,
+//   Y: 10,
+//   Z: 10,
+//   },
+//   departureTime: 50,
+//   },
+//   C: {
+//   drivers: {
+//   X: 6,
+//   Y: 100,
+//   Z: 99,
+//   },
+//   departureTime: 0,
+//   },
+//   D: {
+//   drivers: {
+//   X: 4,
+//   Y: 100,
+//   Z: 5,
+//   },
+//   departureTime: 0,
+//   },
+//   E: {
+//   drivers: {
+//   X: 101,
+//   Y: 5,
+//   Z: 101,
+//   },
+//   departureTime: 0,
+//   },
+//   },
+//   priority: {
+//   F: {
+//   driver: {
+//   Y: 5,
+//   },
+//   departureTime: 0,
+//   },
+//   },
+//   };
+
+
+
+// function toAlgoParser () {
+//   console.log('calling me yes!')
+// }
+
+
+
+// export default toAlgoParser;
