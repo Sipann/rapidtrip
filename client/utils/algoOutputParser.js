e532155f0819536d8c4f801eb317f85bb01ca325
Nicole Carrion Cogo <nicolecogo@gmail.com>
2020-05-07 10:40:52 +0200

    feat: utility file for parsing algorithm output

---
+++ client/utils/algoOutputParser.js
@@ -0,0 +1,77 @@
+//TODO parse this
+// {
+//   X: {
+//     spots: 2,
+//     current: [
+//       { Passenger: 'A', Time: 1 },
+//       { Passenger: 'C', Time: 6 },
+//     ],
+//   },
+//   Z: {
+//     spots: 2,
+//     current: [
+//       { Passenger: 'B', Time: 10 },
+//       { Passenger: 'D', Time: 5 },
+//     ],
+//   },
+//   Y: {
+//     spots: 2,
+//     current: [
+//       { Passenger: 'E', Time: 5 },
+//       { Passenger: 'F', Time: 5 },
+//     ],
+//   },
+// };
+//TODO to this (getting some extra data and merging)
+// const mockResult = [
+//   {
+//     driver: {
+//       id: 1, name: 'Virginie',
+//       departureDate: 1588774296658,
+//       departureLocation: { lat: 41.401648, lng: -2.186230 },
+//       departureTime: '16:30:00'
+//     },
+//     passengers: [
+//       {
+//         id: 2, name: 'Brendan',
+//         departureDate: 1588774296658,
+//         departureLocation: { lat: 41.4019693, lng: -2.1860034 },
+//         departureTime: '15:45:00'
+//       },
+//       {
+//         id: 3, name: 'Anthony',
+//         departureDate: 1588774296658,
+//         departureLocation: { lat: 41.4019695, lng: -2.1862589 },
+//         departureTime: '16:15:00'
+//       },
+//       {
+//         id: 4, name: 'Lello',
+//         departureDate: 1588774296658,
+//         departureLocation: { lat: 41.4019542, lng: -2.1862875 },
+//         departureTime: '16:05:00'
+//       }
+//     ]
+//   },
+//   {
+//     driver: {
+//       id: 5, name: 'John',
+//       departureDate: 1588774443514,
+//       departureLocation: { lat: 41.478622, lng: 2.085599 },
+//       departureTime: '20:45:00'
+//     },
+//     passengers: [
+//       {
+//         id: 6, name: 'Jane',
+//         departureDate: 1588774443514,
+//         departureLocation: { lat: 41.473568, lng: 2.081234 },
+//         departureTime: '19:45:00'
+//       },
+//       {
+//         id: 7, name: 'Jake',
+//         departureDate: 1588774443514,
+//         departureLocation: { lat: 41.475789, lng: 2.085781 },
+//         departureTime: '19:30:00'
+//       }
+//     ]
+//   }
+// ];
