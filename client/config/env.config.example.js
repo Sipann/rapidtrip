a2faf32afead77d247b5a140cb37e1eede6af20d
acandael <info@anthonycandaele.com>
2020-05-07 20:43:04 +0200

    add env.config.example.js

---
+++ client/config/env.config.example.js
@@ -0,0 +1,10 @@
+export default {
+  GOOGLE_API_KEY: '',
+  FIREBASE_API_KEY: '',
+  AUTH_DOMAIN: '',
+  DATABASE_URL: '',
+  PROJECT_ID: '',
+  STORAGE_BUCKET: '',
+  MESSAGING_SENDER_ID: '',
+  APP_ID: '',
+};
