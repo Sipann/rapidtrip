28197e71aa3a16300425bd565d1d6ed9345ab1a8
Nicole Carrion Cogo <nicolecogo@gmail.com>
2020-05-07 10:30:11 +0200

    feat: add more departure info

--- client/App.js
+++ client/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Result from './components/Result';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Result />
     </View>
   );
 }
@@ -13,7 +14,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
 });
