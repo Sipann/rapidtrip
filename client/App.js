23d188a7763b3d9328b1d88f3bbc861c3909f8cd
acandael <info@anthonycandaele.com>
2020-05-09 16:23:36 +0200

    implement bottom tab navigation

--- client/App.js
+++ client/App.js
@@ -4,18 +4,24 @@ import Result from './components/Result';
 import { StyleSheet, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import HomeScreen from './screens/HomeScreen';
+
 import Form from './MVPForm/FormLaunch';
 
 import Colors from './constants/colors';
 
 const Stack = createStackNavigator();
 
-export default function App () {
+const Tab = createMaterialBottomTabNavigator();
+
+export default function App() {
   return (
     <View style={styles.container}>
       <NavigationContainer>
         <Stack.Navigator
-          initialRouteName='MVPForm'
+          initialRouteName="MVPForm"
           screenOptions={{
             headerStyle: {
               backgroundColor: Colors.primary,
@@ -24,21 +30,49 @@ export default function App () {
             headerTitleAlign: 'center',
             headerTitleStyle: {
               fontWeight: 'bold',
-              fontSize: 18
-            }
+              fontSize: 18,
+            },
           }}
         >
           <Stack.Screen
-            name='MVPForm'
+            name="MVPForm"
             component={Form}
-            options={{title: 'Trippy'}}
+            options={{ title: 'Trippy' }}
           />
           <Stack.Screen
-            name='Result'
+            name="Result"
             component={Result}
-            options={{title: 'Car Allocation'}}
+            options={{ title: 'Car Allocation' }}
           />
         </Stack.Navigator>
+      </NavigationContainer>
+      <NavigationContainer>
+        <Tab.Navigator
+          initialRouteName="Feed"
+          activeColor="#fff"
+          barStyle={{ backgroundColor: Colors.secondary }}
+        >
+          <Tab.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              tabBarLabel: 'Home',
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name="home" color={color} size={26} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Notifications"
+            component={HomeScreen}
+            options={{
+              tabBarLabel: 'Settings',
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name="cogs" color={color} size={26} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
       </NavigationContainer>
     </View>
   );
