d3455f1a34e0101ea248b13d2ebadc73bb3b2ca3
Brendan-G5 <brendan.garvey@queensu.ca>
2020-05-09 20:05:56 +0200

    Chore:Homepage set up

--- client/App.js
+++ client/App.js
@@ -1,54 +1,145 @@
-import 'react-native-gesture-handler';
+// import 'react-native-gesture-handler';
+// import React from 'react';
+// import Result from './components/Result';
+// import { StyleSheet, View } from 'react-native';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import Form from './MVPForm/FormLaunch';
+
+// import Colors from './constants/colors';
+
+// const Stack = createStackNavigator();
+
+// export default function App () {
+//   return (
+//     <View style={styles.container}>
+//       <NavigationContainer>
+//         <Stack.Navigator
+//           initialRouteName='MVPForm'
+//           screenOptions={{
+//             headerStyle: {
+//               backgroundColor: Colors.primary,
+//             },
+//             headerTintColor: 'black',
+//             headerTitleAlign: 'center',
+//             headerTitleStyle: {
+//               fontWeight: 'bold',
+//               fontSize: 18
+//             }
+//           }}
+//           >
+//           <Stack.Screen
+//             name='MVPForm'
+//             component={Form}
+//             options={{title: 'Trippy'}}
+//           />
+//           <Stack.Screen
+//             name='Result'
+//             component={Result}
+//             options={{title: 'Car Allocation'}}
+//           />
+//         </Stack.Navigator>
+//       </NavigationContainer>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'stretch',
+//     justifyContent: 'center',
+//   },
+// });
+
 import React from 'react';
-import Result from './components/Result';
-import { StyleSheet, View } from 'react-native';
+import TripsPage from './components/TripsPage'
+import AddTrip from './components/AddTrip'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Form from './MVPForm/FormLaunch';
+import { Text, StyleSheet, Button } from 'react-native';
 
-import Colors from './constants/colors';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const Stack = createStackNavigator();
 
-export default function App () {
+export default function App({navigation}) {
   return (
-    <View style={styles.container}>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName='MVPForm'
-          screenOptions={{
-            headerStyle: {
-              backgroundColor: Colors.primary,
-            },
-            headerTintColor: 'black',
-            headerTitleAlign: 'center',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-              fontSize: 18
-            }
-          }}
-        >
-          <Stack.Screen
-            name='MVPForm'
-            component={Form}
-            options={{title: 'Trippy'}}
-          />
-          <Stack.Screen
-            name='Result'
-            component={Result}
-            options={{title: 'Car Allocation'}}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name ="TripsPage" component ={TripsPage}
+          options={{
+            title: 'RapidTrip',
+            headerRight: () => (
+              <TouchableOpacity
+              onPress={() => navigation.navigate('AddTrip')}>
+              <Text>Create a New Trip</Text>
+              </TouchableOpacity>
+            ),
+          }}/>
+        <Stack.Screen name = "AddTrip" component = {AddTrip} options={{ title: 'Add a Trip' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'stretch',
-    justifyContent: 'center',
+  new: {
+    color: 'black'
+  }
+})
+
+
+const mockedTrips = [
+  {
+    id: 1,
+    title: 'London Roadtrip',
+    date: new Date(2014, 6, 5),
   },
-});
+  {
+    id: 2,
+    title: 'Amsterdam Weekend',
+    date: new Date(2013, 12, 7),
+  },
+  {
+    id: 3,
+    title: 'Weekend at Munich',
+    date: new Date(2016, 11, 7),
+  },
+  {
+    id: 4,
+    title: 'Camping Weekend',
+    date: new Date(2016, 12, 8),
+  },
+  {
+    id: 5,
+    title: 'Trip to Rome',
+    date: new Date(2017, 2, 8),
+  },
+  {
+    id: 6,
+    title: 'Trip to Barcelona',
+    date: new Date(2020, 5, 10),
+  },
+  {
+    id: 7,
+    title: 'Trip to Chamonix',
+    date: new Date(2020, 5, 11),
+  },
+  {
+    id: 8,
+    title: 'Trip to Andora',
+    date: new Date(2020, 5, 15),
+  },
+  {
+    id: 9,
+    title: 'Trip to Milan',
+    date: new Date(2020, 5, 20),
+  },
+  {
+    id: 10,
+    title: 'Trip to Rome',
+    date: new Date(2020, 7, 20),
+  },
+];
---
+++ client/components/TripsPage.js
@@ -0,0 +1,154 @@
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, Button, Image } from 'react-native';
+import { TouchableOpacity, ScrollView } from 'react-native-gesture-handler';
+
+export default function TripsPage ({navigation}) {
+
+  const tripList = mockedTrips;
+  const upcomingTrips = tripList.filter(trip => trip.date >= Date.now()).sort((a,b) => a.date - b.date);
+  const pastTrips = tripList.filter(trip => trip.date < Date.now()).sort((a,b) => b.date - a.date);
+
+  const [currentList, setCurrentList] = useState(upcomingTrips);
+
+
+  function Item ({ trip }) {
+    return (
+      <TouchableOpacity style={styles.item} >
+        <Text style={styles.tripname}>{trip.title}</Text>
+        <Image style={styles.photo} source={require('../assets/carClipArt.jpg')}/>
+        <Text style={styles.tripdate}>{trip.date.toUTCString()}</Text>
+      </TouchableOpacity>
+    );
+  }
+
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Welcome USERNAME!</Text>
+      <View style={styles.buttonGroup}>
+        <TouchableOpacity onPress={() => { setCurrentList(upcomingTrips); }} >
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>
+            UPCOMING
+            </Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => { setCurrentList(pastTrips); }} >
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>
+            PAST TRIPS
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+      <ScrollView style={styles.list}>
+        {
+          currentList.map(trip =>
+            <Item
+              key={trip.id}
+              trip={trip}
+            />)
+        }
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+  },
+  buttonGroup: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    margin: 20
+  },
+  button: {
+    backgroundColor: 'blue',
+    margin: 5,
+    borderRadius: 10
+  },
+  buttonText: {
+    margin: 10,
+    color: 'white'
+  },
+  list: {
+    height: '80%'
+  },
+  item: {
+    margin: 20,
+    backgroundColor: 'lightgray',
+    padding: 10
+  },
+  tripname: {
+    fontSize: 30
+  },
+  photo: {
+    height: 100,
+    width: '100%',
+    marginTop:5
+  },
+  welcome: {
+    margin:'auto',
+    textAlign: 'center'
+  }
+});
+
+
+
+const mockedTrips = [
+  {
+    id: 1,
+    title: 'London Roadtrip',
+    date: new Date(2014, 6, 5),
+  },
+  {
+    id: 2,
+    title: 'Amsterdam Weekend',
+    date: new Date(2013, 12, 7),
+  },
+  {
+    id: 11,
+    title: 'Amsterdam Weekend',
+    date: new Date(2013, 12, 7),
+  },
+  {
+    id: 3,
+    title: 'Weekend at Munich',
+    date: new Date(2016, 11, 7),
+  },
+  {
+    id: 4,
+    title: 'Camping Weekend',
+    date: new Date(2016, 12, 8),
+  },
+  {
+    id: 5,
+    title: 'Trip to Rome',
+    date: new Date(2017, 2, 8),
+  },
+  {
+    id: 6,
+    title: 'Trip to Barcelona',
+    date: new Date(2020, 5, 10),
+  },
+  {
+    id: 7,
+    title: 'Trip to Chamonix',
+    date: new Date(2020, 5, 11),
+  },
+  {
+    id: 8,
+    title: 'Trip to Andora',
+    date: new Date(2020, 5, 15),
+  },
+  {
+    id: 9,
+    title: 'Trip to Milan',
+    date: new Date(2020, 5, 20),
+  },
+  {
+    id: 10,
+    title: 'Trip to Rome',
+    date: new Date(2020, 7, 20),
+  },
+];
