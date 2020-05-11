2adee6a60ac91d73cabd929ed33950c7bd165bbf
Nicole Carrion Cogo <nicolecogo@gmail.com>
2020-05-11 18:45:45 +0200

    chore: implement getUser function

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -2,7 +2,42 @@ const db = require('../database');
 const User = db.models.Person;
 
 module.exports.getUser = async ctx => {
-  ctx.body = {};
+  // create response object
+  let res = {};
+  // store user_id passed as parameter
+  const userId = ctx.params.user_id;
+
+  try {
+    // get User instance associated to the provided id
+    const user = await User.findByPk(userId);
+    if (!user) throw {
+      status: 404,
+      message: 'No user found with provided id'
+    };
+
+    // populate response object
+    res.ok = true;
+    res.body = {};
+    res.body.user = user.toJSON();
+    res.body.trips = [];
+    // set response status
+    ctx.status = 200;
+    
+    // get all trips associated to the user (if any)
+    const trips = await user.getTrips();
+    if (trips.length > 0) {
+      res.body.trips = trips.toJSON();
+    }
+  } catch (error) {
+    // populate response object
+    res.ok = false;
+    res.error = error.message;
+    // set response status
+    ctx.status = error.status || 400;
+  } finally {
+    // send response
+    ctx.body = res;
+  }
 };
 
 module.exports.createUser = async ctx => {
