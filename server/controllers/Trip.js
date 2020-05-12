8bf9f49fc60834c35e7c078e5b9cadf3f8cd71c0
Nicole Carrion Cogo <nicolecogo@gmail.com>
2020-05-12 17:26:45 +0200

    chore: implement remove user from trip function

--- server/controllers/Trip.js
+++ server/controllers/Trip.js
@@ -12,7 +12,63 @@ module.exports.deleteTrip = async ctx => {
 };
 
 module.exports.removeUser = async ctx => {
-  ctx.body = {};
+  // create response object
+  let res = {};
+  // store trip_id passed as parameter
+  const tripId = ctx.params.trip_id;
+  // store user_email passed as parameter
+  const userId = ctx.params.user_email;
+
+  try {
+    // get Trip instance associated to the provided id
+    const trip = await Trip.findByPk(tripId);
+    if (!trip) throw {
+      status: 404,
+      message: 'No trip found with provided id'
+    };
+
+    // get Trip's participant associated to the provided id
+    const participants = await trip.getParticipants({ where: { email: userId } });
+    if (participants.length <= 0) throw {
+      status: 404,
+      message: 'The requested user is not on this trip'
+    };
+
+    // check if participant is admin
+    const participant = participants[0].toJSON();
+    if (participant.Participant.is_admin) throw {
+      status: 404,
+      message: 'Cannot remove the trip admin'
+    };
+
+    const number = await trip.removeParticipant(participants[0]);
+    if (number <= 0) throw {
+      status: 500,
+      message: 'It was not possible to remove the requested user from the trip'
+    };
+
+    // retrieve updated list of participants for response
+    const updatedParticipants = await trip.getParticipants();
+    if (updatedParticipants.length < 0) throw {
+      status: 500,
+      message: 'Not possible to retrieve participants list'
+    };
+
+    // populate response object
+    res.ok = true;
+    res.body = parseParticipants(updatedParticipants.map(participant => participant.toJSON()));
+    // set response status
+    ctx.status = 200;
+  } catch (error) {
+    // populate response object
+    res.ok = false;
+    res.error = error.message;
+    // set response status
+    ctx.status = error.status || 400;
+  } finally {
+    // send response
+    ctx.body = res;
+  }
 };
 
 module.exports.includeUser = async ctx => {
