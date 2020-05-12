e8b2f33f5576dc9466ef4610d44e4fe57a4d70d8
Nicole Carrion Cogo <nicolecogo@gmail.com>
2020-05-12 21:52:22 +0200

    chore: implement update car allocation function

--- server/controllers/Trip.js
+++ server/controllers/Trip.js
@@ -2,6 +2,8 @@ const db = require('../database');
 const Trip = db.models.Trip;
 const User = db.models.Person;
 const Location = db.models.Location;
+const Participant = db.models.Participant;
+const Car = db.models.Car;
 const { parseParticipants } = require('./utils');
 
 module.exports.deleteTrip = async ctx => {
@@ -207,5 +209,84 @@ module.exports.updateInfo = async ctx => {
 };
 
 module.exports.updateCars = async ctx => {
-  ctx.body = {};
+  // create response object
+  let res = {};
+  // store trip_id passed as parameter
+  const tripId = ctx.params.trip_id;
+  // store trip object passed in body
+  const { cars } = ctx.request.body;
+
+  // start a transaction
+  const t = await db.transaction();
+
+  try {
+    // get Trip instance associated to the provided id
+    const oldTrip = await Trip.findByPk(tripId);
+    if (!oldTrip) throw {
+      status: 404,
+      message: 'No trip found with provided id'
+    };
+
+    if (cars.length <= 0) throw {
+      status: 400,
+      message: 'No car information provided'
+    };
+
+    let carIntances = [];
+    for (let car of cars) {
+      // create and save Car instances
+      const carIntance = await Car.create(car, { transaction: t });
+      carIntances.push(carIntance);
+      for (let passId of car.passengers) {
+        // retrieve Participant instances from ids
+        const passInstance = await Participant.findAll(
+          { where: { person_id: passId, trip_id: oldTrip.get('id') } }
+        );
+        if (passInstance.length <= 0) throw {
+          status: 404,
+          message: 'No user found with provided id'
+        };
+        // associate Cars to Passengers
+        passInstance[0].setCar(carIntance, { transaction: t });
+        // update driver        
+        if (passId === car.driver_id)
+          passInstance[0].update({ is_driver: true }, { transaction: t });
+      }
+    }
+    // set new car allocation
+    await oldTrip.setCars(carIntances, { transaction: t });
+
+    // commit the transaction
+    await t.commit();
+    
+    // retrieve updated car allocation with passenger information
+    const newTrip = await Trip.findByPk(oldTrip.get('id'), {
+      include: [{
+        model: Car,
+        as: 'cars',
+        include: [{
+          model: Participant,
+          as: 'passengers'
+        }]
+      }]
+    });
+    const newCars = newTrip.cars;
+
+    // populate response object
+    res.ok = true;
+    res.body = newCars;
+    // set response status
+    ctx.status = 200;
+  } catch (error) {
+    // populate response object
+    res.ok = false;
+    res.error = error.message;
+    // set response status
+    ctx.status = error.status || 400;
+    // rollback the transaction
+    await t.rollback();
+  } finally {
+    // send response
+    ctx.body = res;
+  }
 };
--- server/controllers/utils.js
+++ server/controllers/utils.js
@@ -7,7 +7,8 @@ module.exports.parseParticipants = function (participants) {
       departure_time: participant_info.departure_time,
       is_admin: participant_info.is_admin,
       departure_location_id: participant_info.departure_location_id,
-      car_id: participant_info.car_id
+      car_id: participant_info.car_id,
+      is_driver: participant_info.is_driver
     };
   });
 };
