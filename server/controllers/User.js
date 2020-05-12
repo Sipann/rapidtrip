03f16d9dfecbbceb16de3b05e0148da9979d621c
Nicole Carrion Cogo <nicolecogo@gmail.com>
2020-05-12 11:11:15 +0200

    chore: implement update user info function

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -114,6 +114,38 @@ module.exports.removeUser = async ctx => {
 };
 
 module.exports.updateInfo = async ctx => {
+  // create response object
+  let res = {};
+  // store user_id passed as parameter
+  const userId = ctx.params.user_id;
+  // store user object passed in body
+  const userInfo = ctx.request.body;
+
+  try {
+    // get User instance associated to the provided id
+    const oldUser = await User.findByPk(userId);
+    if (!oldUser) throw {
+      status: 404,
+      message: 'No user found with provided id'
+    };
+
+    const user = await oldUser.update(userInfo, { fields: ['id', 'name', 'picture'] });
+
+    // populate response object
+    res.ok = true;
+    res.body = user.toJSON();
+    // set response status
+    ctx.status = 200;
+  } catch (error) {
+    // populate response object
+    res.ok = false;
+    res.error = error.message;
+    // set response status
+    ctx.status = error.status || 400;
+  } finally {
+    // send response
+    ctx.body = res;
+  }
 };
 
 module.exports.createTrip = async ctx => {
