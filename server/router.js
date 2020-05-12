1da7ad6b737e1d984ace6347ba0374d6f4e2d425
Nicole Carrion Cogo <nicolecogo@gmail.com>
2020-05-12 22:24:18 +0200

    fix: changed route method for updating user info

--- server/router.js
+++ server/router.js
@@ -6,7 +6,7 @@ router.get('/user/:user_email', User.getUser);
 
 router.post('/trip/:user_email', User.createTrip);
 router.post('/user', User.createUser);
-router.put('/trip/:trip_id/:user_email', Trip.includeUser);
+router.post('/trip/:trip_id/:user_email', Trip.includeUser);
 
 router.delete('/trip/:trip_id', Trip.deleteTrip);
 router.delete('/user/:user_email', User.removeUser);
